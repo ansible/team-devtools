aeabae5d4da7b1c709a90ceafe9ec5f6a78d349f
Kate Case <this.is@katherineca.se>
2025-09-22 10:49:36 -0400

    Drop reliance on network requests

--- test/test_forum_post.py
+++ test/test_forum_post.py
@@ -17,40 +17,33 @@ spec.loader.exec_module(forum_post)
 @pytest.fixture
 def post_instance() -> forum_post.Post:
     """A Post instance for testing."""
-    return forum_post.Post(
+    post = forum_post.Post(
         project="ansible/molecule",
         release="v25.3.1",
         forum_api_key="unused",
         forum_user="unused",
     )
-
-
-def test_post_params(post_instance: forum_post.Post) -> None:
-    """Test that derived parameters are generated correctly."""
-    assert post_instance.project_short == "molecule"
-
-
-def test_get_release_notes(post_instance: forum_post.Post) -> None:
-    """Test that release notes are generated correctly."""
-    release_notes, created = post_instance._get_release_notes()  # noqa: SLF001
-
-    assert (
-        release_notes
-        == """## Bugfixes\r
-\r
-- Fix molecule matrix with no scenario name. (#4400) @Qalthos\r
-"""
+    # prefill values that need network access.
+    post.category_id = "18"
+    post.created = "2025-02-19T12:53:51Z"
+    post.release_notes = (
+        "## Bugfixes\r\n\r\n- Fix molecule matrix with no scenario name. (#4400) @Qalthos\r\n"
     )
-    assert created == "2025-02-19T12:53:51Z"
+    return post
 
 
 def test_prepare_post(post_instance: forum_post.Post) -> None:
     """Test that discourse post payload is generated correctly."""
-    # Set some arbitrary values to missing parameters
-    post_instance.category_id = "10"
-    post_instance.created = "A date"
-    post_instance.release_notes = "Release notes go here"
-
     payload = post_instance._prepare_post()  # noqa: SLF001
-    assert payload["title"] == "Release Announcement: molecule v25.3.1"
-    assert payload["tags"] == ["devtools", "release-management"]
+    release_notes = forum_post.POST_MD.format(
+        project_short="molecule",
+        release=post_instance.release,
+        release_notes=post_instance.release_notes,
+    )
+    assert payload == {
+        "title": "Release Announcement: molecule v25.3.1",
+        "raw": release_notes,
+        "category": post_instance.category_id,
+        "created_at": post_instance.created,
+        "tags": ["devtools", "release-management"],
+    }
